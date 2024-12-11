2ceb6052c6829197e0f25395a0f3b4ea571cd54b
ml1413 <ml1413sam@gmail.com>
2024-12-11 16:53:24 +0200

    remove class ModelHistoryStorageTypeToken

--- app/src/main/java/com/multiplying_numbers/data/storage/sharedPref/StorageSharedPrefImpl.kt
+++ app/src/main/java/com/multiplying_numbers/data/storage/sharedPref/StorageSharedPrefImpl.kt
@@ -62,8 +62,6 @@ class StorageSharedPrefImpl @Inject constructor(
     }
 
     private fun getModelFromStorage(key: String): ModelHistoryStorage? {
-        class ModelHistoryStorageTypeToken : TypeToken<ModelHistoryStorage>()
-
         val json = sharedPreferences.getString(key, null) ?: return null
         val gson = Gson()
         val modelHistoryStorage: ModelHistoryStorage? =
